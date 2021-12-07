e5261b2e7e65eab68d95f867c22160edf8eadc03
Lukas Hagman <lukas@geoguessr.com>
2021-12-07 21:04:40 +0100

    day 7

---
+++ 7/Program.cs
@@ -0,0 +1,38 @@
+﻿// Today, I choose violence
+// Not because I must
+// But because I can
+// - Albert Einstein (probably)
+
+using System.Linq;
+
+var input = System.IO.File.ReadAllLines("input.txt")
+    .First()
+    .Split(',')
+    .Select(x => Convert.ToInt32(x))
+    .OrderBy(x => x);
+
+Console.WriteLine(
+    string.Join(
+        ", ", 
+        (new [] { true, false })
+            .Select(part1 =>
+            part1
+                ? input
+                    .Select(x => Math.Abs(
+                        (input.Count() % 2 == 1
+                            ? input.ElementAt(input.Count() / 2)
+                            : (input.ElementAt((input.Count() - 1) / 2)
+                                + input.ElementAt(input.Count() / 2))
+                                / 2)
+                        - x
+                        ))
+                    .Sum()
+                : Enumerable
+                    .Range(input.Min(), input.Max() - input.Min())
+                    .Select(x => 
+                        input
+                            .Select(y => Enumerable.Range(1, Math.Abs(x - y)).Sum())
+                            .Sum())
+                    .OrderBy(x => x)
+                    .First())));
+
